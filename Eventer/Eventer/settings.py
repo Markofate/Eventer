a09b3d7ddf60cdca20a3696ae8d27d75be4858c3
kemalkunt <114948156+Markofate@users.noreply.github.com>
2024-02-03 09:24:15 +0300

    Update settings.py

--- Eventer/Eventer/settings.py
+++ Eventer/Eventer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_afkp9e_pvfr%lco4o_fo5rcssy3^f%2oi8c7bd^w*_s78)5#b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
